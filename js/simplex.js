78aed8552ef8d61977254bb469fed01338517e05
TaahSene <tay.sene@gmail.com>
2016-04-02 11:06:26 -0300

    Cabeçario da Matrix Inicial

--- js/simplex.js
+++ js/simplex.js
@@ -1,10 +1,34 @@
 
 function simplex() {
+ //Chama a função MontarMatrix
   montamatrix();
 }
 
 function montamatrix() {
-  var montamatrix
-  var inputZ = document.getElementById('zFunction').value.split(';');
-  console.log(inputZ);
+
+  //Pega o valores da função
+  var z = document.getElementById('zFunction').value.split(';');
+
+  //Pega os campos de restrição
+  var rest = document.querySelectorAll(".form-control");
+
+  //Gera a Matrix
+  matrix = [[]];
+
+  //Adiciona A primeira Linha a Palavra "Linha"
+  matrix[0].push('Linha');
+  
+  //Adiona as variaveis x ao cabeçario
+  for(var i = 1; i <= z.length; i++)
+    matrix[0].push('x' + i);
+
+ //Adiciona as variaveis de folga ao cabecalho
+  for(var i = 1; i <=rest.length; i++)
+    matrix[0].push('f' + i);
+
+ //Adiocina o "B" na ultima colunma
+  matrix[0][matrix[0].length] = 'b'
+
+  console.log(matrix);
+
 }
---
+++ js/style.js
@@ -0,0 +1,6 @@
+//Adiona os campos de restrição
+$(document).ready(function(){
+	 $("#addRestriction").click(function(){
+  		$( "#restrictionVariable" ).append('<input type="text" class="form-control" name="restrictionVariable"/></br>' );
+  	});
+});
