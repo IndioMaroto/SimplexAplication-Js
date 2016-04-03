8c036524be338e9c46c4e99805c681115d509e8d
TaahSene <tay.sene@gmail.com>
2016-04-03 12:35:41 -0300

    Gera Matrix Inicial

--- js/simplex.js
+++ js/simplex.js
@@ -4,31 +4,86 @@ function simplex() {
   montamatrix();
 }
 
-function montamatrix() {
+function montamatrix()
+{
+  // Input Z
+  var inputZ = document.getElementById('zFunction').value.split(';');
 
-  //Pega o valores da função
-  var z = document.getElementById('zFunction').value.split(';');
+  // Restrições
+  var rest = document.querySelectorAll('#subjects .form-group');
 
-  //Pega os campos de restrição
-  var rest = document.querySelectorAll(".form-control");
+  // Cria a matriz do simplex
+  matriz = [[]];
 
-  //Gera a Matrix
-  matrix = [[]];
+  // Primeira linha: 'cabeçalho'
+  matriz[0][0] = 'Linha';
 
-  //Adiciona A primeira Linha a Palavra "Linha"
-  matrix[0].push('Linha');
-  
-  //Adiona as variaveis x ao cabeçario
-  for(var i = 1; i <= z.length; i++)
-    matrix[0].push('x' + i);
+  // Preenche as variaveis 'x'
+  for(var i = 1; i <= inputZ.length; i++)
+    matriz[0].push('x' + i);
 
- //Adiciona as variaveis de folga ao cabecalho
-  for(var i = 1; i <=rest.length; i++)
-    matrix[0].push('f' + i);
+  // Preenche as variaveis de 'f'
+  for(var i = 1; i <= rest.length; i++)
+    matriz[0].push('f' + i);
 
- //Adiocina o "B" na ultima colunma
-  matrix[0][matrix[0].length] = 'b'
+  // Preenche as variaveis de 'b'
+  matriz[0][matriz[0].length] = 'b';
 
-  console.log(matrix);
+  var count = 1;
 
+  // Para cada restrição, teremos uma linha
+  for(var i = 1; i <= rest.length; i++)
+  {
+    // Adiciona a linha fN na primeira coluna
+    matriz.push(['f' + i]);
+
+    count = 1;
+
+    // Valores das variáveis básicas
+    var subjectVars = rest[i-1].querySelectorAll('input')[0].value.split(';');
+
+    // Preenche as variáveis xN
+    for(var j = 0; j < inputZ.length; j++)
+    {
+      if(matriz[j])
+        matriz[i][count++] = Number(subjectVars[j]);
+      else
+        matriz[i][count++] = 0;
+    }
+
+    // Preenche os valores de fN
+    for(var j = 0; j < count.length; j++)
+    {
+      matriz[i][count] = Number((matriz[i][0] == matriz[0][count] ? 1 : 0));
+      count++;
+    }
+
+    // Preenche o valor de 'b'
+    matriz[i][count] = Number(rest[i-1].querySelectorAll('input')[1].value);
+  }
+
+  // E também uma linha final 'Z'
+  matriz.push(['Z']);
+
+  // Reseta o contador
+  count = 1;
+
+  // Preenche as variáveis xN
+  for(var i = 0; i < inputZ.length; i++)
+  {
+    if(inputZ[i])
+      matriz[matriz.length - 1][count++] = Number(-1 * inputZ[i]);
+    else
+      matriz[matriz.length - 1][count++] = 0;
+  }
+
+  // Preenche os valores de fN
+  for(var j = 0; j < rest.length; j++)
+    matriz[matriz.length - 1][count++] = 0;
+
+  // Preenche o valor de b
+  matriz[matriz.length - 1][count] = 0;
+
+ //Retorna Matrix Montada
+  console.log(matriz);
 }
--- js/style.js
+++ js/style.js
@@ -1,6 +1,28 @@
-//Adiona os campos de restrição
-$(document).ready(function(){
-	 $("#addRestriction").click(function(){
-  		$( "#restrictionVariable" ).append('<input type="text" class="form-control" name="restrictionVariable"/></br>' );
-  	});
-});
+var subjectCount = 0;
+
+function addSubjects()
+{
+  // Cria uma nova restrição
+  var newSubject = document.createElement('div');
+
+  // Cria a parte superior do input
+  var subjectInput = "<div class='input-group'>";
+  subjectInput += "<div class='input-group-addon'>f" + (subjectCount + 1) + "</div>";
+  subjectInput += "<input type='text' class='form-control id='subject" + subjectCount + "'/>";
+  subjectInput += "</div>";
+
+  // Cria a parte inferior do input
+  var subjectSecondInput = "<div class='input-group'>";
+  subjectSecondInput += "<div class='input-group-addon'>&le;</div>";
+  subjectSecondInput += '<input type="text" class="form-control" id="subjectTyped' + subjectCount + '" value="0"/>';
+  subjectSecondInput += "</div>";
+
+  // Junta os campos
+  newSubject.innerHTML = "<div class='form-group'>" + subjectInput + subjectSecondInput + "</div>";
+
+  // Adiciona a restrição ao corpo da página
+  document.getElementById("resr").appendChild(newSubject);
+
+  // Incrementa a quantidade de subjects
+  subjectCount++;
+}
